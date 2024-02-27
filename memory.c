94e836233227cde881c10b50614a09003adef138
kingsyung <kakpanyung@gmail.com>
2024-02-26 16:42:32 -0800

    memory.c

--- memory.c
+++ memory.c
@@ -1,8 +1,8 @@
 #include "shell.h"
 
 /**
- * bfree - frees a pointer and NULLs the address
- * @ptr_addr: address of the pointer to free
+ * bfree - frees pointers and NULLs the address
+ * @ptr_addr: address of the pointers to free
  *
  * Return: 1 if freed, otherwise 0.
  */
